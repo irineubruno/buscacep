687dfcabf3eea58ac8fbe2b82ba0aa5b850bd2d3
Bruno Irineu <irineubruno@hotmail.com>
2022-09-22 21:29:44 -0400

    v.1

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:buscacep/view/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    routes: {
+      '/':(_) => Home(),
+    },
+  ));
+}
+
